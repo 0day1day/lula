5847147a1b0265c4fc1462c014cf81e03d17b37b
Fernando Mercês <nandu88@gmail.com>
2013-04-10 08:15:29 -0300

    added virustotal username

--- vt-query.sh
+++ vt-query.sh
@@ -4,12 +4,13 @@ source lula.conf
 
 [ $# -eq 1 ] || exit 
 
-sha1=$1
-apikey=$virustotal_apikey
+sha1="$1"
+apikey="$virustotal_apikey"
+user="$virustotal_user"
 file_retr_url='https://www.virustotal.com/vtapi/v2/file/report'
 tmpfile=$(mktemp)
 
-wget -q --post-data="resource=$sha1&apikey=$apikey" $file_retr_url -O $tmpfile
+wget -q --post-data="whoami=$user&resource=$sha1&apikey=$apikey" $file_retr_url -O $tmpfile
 
 for i in $(jshon -e scans < $tmpfile | grep '{$' | tr -d ' ":{' | sort); do
 	echo -n "$i: "
